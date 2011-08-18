6b13a79546f9ec4b99e85304236a5a7a2261f06c
Max Williams <max@cheetara.office.whitebearyard.com>
2011-08-18 12:55:34 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notifiy::Application.config.secret_token = '13daba40bff6fe75148cf6b56f18d1ff4292cff662d2ec7fb17154d9be8f9f278dc076835c73e16b264b9f3d72bdb384a1735e879a6ba4ae34d37dc77179d73b'
